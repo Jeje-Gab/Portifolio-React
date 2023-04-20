e2c34b60260bba21cfb47c9bbba378ae978419b8
Jeje-Gab <gabrieldej50@gmail.com>
2023-04-20 00:17:36 -0300

    Ajustado e centralizado informações e iniciado mudanças relacionadas ao nav-links

--- meu-projeto/src/Components/College/College.jsx
+++ meu-projeto/src/Components/College/College.jsx
@@ -6,6 +6,9 @@ function College() {
     <div className="ItensCollege space">
       <div>
         <div className="BoxCollege">
+          <div className="TitleCollege">
+            <h1>Formação</h1>
+          </div>
           <div className="College">IFSC - 2017 - 2021</div>
           <div className="College">Unoesc - 2022</div>
           <div className="College">JS</div>
--- meu-projeto/src/Components/Navbar/Navbar.jsx
+++ meu-projeto/src/Components/Navbar/Navbar.jsx
@@ -13,32 +13,34 @@ function Navbar() {
     <nav className="navbar">
       <ul className="navbar-nav mr-auto nav-ul">
         <li className="nav-item active">
-          <a className="nav-link" href="#">
+          <a 
+          data-scroll="home"
+          className="nav-link">
             Home
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">
+          <a className="nav-link" href="#sec2">
             Sobre
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">
+          <a className="nav-link" href="#sec3">
             Experiencia Profissional
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">
+          <a className="nav-link" href="#sec4">
             Skills
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">
+          <a className="nav-link" href="#sec5">
             Formação
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">
+          <a className="nav-link" href="#sec6">
             Contato
           </a>
         </li>
